dad5d8a7ee1f78c43769c2cccaea8f072ee9aaeb
Eva <godnebeles@gmail.com>
2025-05-21 11:07:48 +0300

    Implement match timer and match timer UI

--- Assets/Scripts/Core/MatchmakingComponents/MatchTimer.cs
+++ Assets/Scripts/Core/MatchmakingComponents/MatchTimer.cs
@@ -1,25 +1,56 @@
+using System;
 using Fusion;
+using UnityEngine;
 
 namespace Core.MatchmakingComponents
 {
     public class MatchTimer : NetworkBehaviour
     {
-        [Networked] public float TimeRemaining { get; set; }
+        [Networked]
+        private TickTimer NetworkTimer { get; set; }
 
+        [SerializeField]
+        private float matchDurationSeconds = 180f;
+
+        public event Action<float> TimerUpdatedEvent;
+        public event Action TimerEndedEvent;
+
+        public bool HasEnded { get; private set; }
+        
         public override void Spawned()
         {
             if (HasStateAuthority)
             {
-                TimeRemaining = 120;
+                StartMatchTimer();
             }
         }
 
         public override void FixedUpdateNetwork()
         {
-            if (!HasStateAuthority)
-                return;
+            if (HasEnded) return;
 
-            TimeRemaining -= Runner.DeltaTime;
+            if (NetworkTimer.Expired(Runner))
+            {
+                HasEnded = true;
+                TimerEndedEvent?.Invoke();
+            }
+            else
+            {
+                float timeLeft = NetworkTimer.RemainingTime(Runner) ?? 0f;
+                TimerUpdatedEvent?.Invoke(timeLeft);
+            }
+        }
+
+        public void StartMatchTimer()
+        {
+            if (!HasStateAuthority) return;
+            NetworkTimer = TickTimer.CreateFromSeconds(Runner, matchDurationSeconds);
+            HasEnded = false;
+        }
+
+        public float? GetRemainingTime()
+        {
+            return NetworkTimer.RemainingTime(Runner);
         }
     }
 }
